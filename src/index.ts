d2147d8689400b62730fe046e3bab769bff49b8e
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-16 17:59:42 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 let notaTotal, notaPractica, notaProblemas, notaTeorica: number;
-let nombreAlumno: string = "";
+let nombreAlumno;
 
 while ((nombreAlumno !=="")) {
-  nombreAlumno = String(prompt("Ingrese nombre del alumno:"));
+  nombreAlumno = prompt("Ingrese nombre del alumno:"));
   console.log("Nombre del alumno: " + nombreAlumno);
   notaPractica = Number(prompt("Ingrese nota de practica"));
   console.log("Su nota practica es: " + notaPractica);
   notaProblemas = Number(prompt("Ingrese nota de problemas:"));
   console.log("Su nota de problemas es: " + notaProblemas);
-  notaTeorica = Number(prompt("Ingrese nota teorica" + notaTeorica));
+  notaTeorica = Number(prompt("Ingrese nota teorica"));
   console.log("Su nota teorica es: " + notaTeorica);
 
 if (notaPractica <= 10 && notaProblemas <= 10 && notaTeorica <= 10) {
